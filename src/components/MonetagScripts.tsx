8e9b377a3a68ae2e875eb24dcdc882c1aee9d572
koushik-parimi24 <koushikparimi21@gmail.com>
2025-11-05 21:24:46 +0530

    new domain update

--- src/components/MonetagScripts.tsx
+++ src/components/MonetagScripts.tsx
@@ -2,24 +2,24 @@ import { useEffect } from "react";
 
 const MonetagScripts: React.FC = () => {
   useEffect(() => {
-    // 🟣 Push Notification Tag
+    // 🟣 Updated Push Notification Tag
     const pushScript = document.createElement("script");
-    pushScript.src = "https://3nbf4.com/act/files/tag.min.js?z=10143230";
+    pushScript.src = "https://3nbf4.com/act/files/tag.min.js?z=10146225";
     pushScript.async = true;
     pushScript.setAttribute("data-cfasync", "false");
     document.body.appendChild(pushScript);
 
-    // 🟠 OnClick (Popunder) Tag
+    // 🟠 Updated OnClick (Popunder) Tag
     const popunderScript = document.createElement("script");
     popunderScript.textContent = `
       (function(s){
-        s.dataset.zone='10143226',
+        s.dataset.zone='10146220',
         s.src='https://al5sm.com/tag.min.js'
       })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
     `;
     document.body.appendChild(popunderScript);
 
-    // Cleanup when unmounted
+    // Cleanup on unmount
     return () => {
       document.body.removeChild(pushScript);
       document.body.removeChild(popunderScript);
